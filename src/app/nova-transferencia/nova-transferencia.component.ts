aed6d0a5f5600410f1e9f4489483ce379eaa93c1
Carlos Alberto <carllos.seg@gmail.com>
2022-07-19 15:56:45 -0300

    Exportanto dados do componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, Output } from "@angular/core";
+import { EventEmitter } from "@angular/core";
 
 @Component({
   selector: 'nova-transferencia',
@@ -7,10 +8,28 @@ import { Component } from "@angular/core";
 })
 export class NovaTransferenciaComponent {
 
+ /* Reponsável por enviar os dados para outros componentes */
+@Output() aoTransferir = new EventEmitter<any>();        // Através dessa classe EventEmitter eu consigo propagar esse dado.
+
+
+
+valor: number;
+destino: number;
+
+
 
 
 transferir() {
-  console.log('solicitada nova transferência')
+  console.log('solicitada nova transferência');
+ /*  const valorEmitir = {valor: this.valor, destino: this.destino}
+  this.aoTransferir.emit(valorEmitir); */
+   this.aoTransferir.emit({ valor: this.valor, destino: this.destino});     //Emitindo os dados
+   this.limparCampor();                                                     // Limpado os campos
+}
+
+limparCampor() {
+  this.valor = 0;
+  this.destino = 0;
 }
 
 
